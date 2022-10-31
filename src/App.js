0790f12d57412aefd4d3816d04c60838e1c5fe39
Daniel DeFrance <daniel.defrance@montana.edu>
2022-10-31 15:09:21 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Counter from "./components/Counter";
+import MyGitHub from "./components/MyGitHub";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Hello React!
+      </h1>
+      <h2>
+        Daniel DeFrance - h48c334
+      </h2>
+
+      <hr />
+      <Counter incBy={1}/><hr />
+      <Counter incBy={2} decBy={3}/><hr />
+      <MyGitHub /><hr />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react"
+
+const Counter = (props) => {
+    console.log(props)
+
+    const [countState, setCountState] = useState(77)
+
+    function incCount(){
+        setCountState(countState + props.incBy)
+    }
+
+    // just make an object for styles:
+    const countStyle = {
+        fontSize: "2em"
+    }
+
+    // function decCount(){
+    //     setCountState(countState - props.decBy)
+    // }
+    return (
+        <div>
+            <div id='c' style={countStyle}>{countState}</div>
+            <button onClick={incCount}>+{props.incBy}</button>
+            {/* {props.decBy && <button onClick={decCount}>-{props.decBy}</button>} */}
+        </div>
+    )
+}
+export default Counter;
